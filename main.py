fca20a62d0f385b4fc754f4f2c2d751f3d57ce76
Typezera <willianbardela2131@hotmail.com>
2026-03-20 16:44:51 -0300

    feat: criando a tela do jogo

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import pygame
 
-pygame 
+pygame.init()
+
+largura = 800
+altura = 600
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Obstaculo Demo")
+
+inicio = True
+
+while inicio:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            inicio = False
+
+    tela.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+pygame.quit()
